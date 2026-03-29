b232084ae8a554de3478bc46a1a4534729f40c25
Harsha Vardhan Konisa <harshavardhankonisa@gamil.com>
2026-03-29 12:02:53 +0530

    setup: add docker local and prod setup with prettier drizzle and next-auth

---
+++ src/api/config/db.ts
@@ -0,0 +1,15 @@
+import { drizzle } from 'drizzle-orm/node-postgres'
+import { Pool } from 'pg'
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl:
+    process.env.NODE_ENV === 'production'
+      ? { rejectUnauthorized: false }
+      : false,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 5000,
+})
+
+export const db = drizzle({ client: pool })
